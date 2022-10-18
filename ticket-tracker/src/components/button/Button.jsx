f6ca2797c58ca5213969f629629623c3feca98c8
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-10-18 14:52:58 +0100

    Corrected minor format error.

--- ticket-tracker/src/components/button/Button.jsx
+++ ticket-tracker/src/components/button/Button.jsx
@@ -1,13 +1,13 @@
 import "./Button.scss";
 
-const Button = ((props) => {
+const Button = (props) => {
 
     const { buttonText, buttonValue } = props;
 
     return(
         <button value={buttonValue}>{buttonText}</button>
     )
-});
+};
 
 
 export default Button;
