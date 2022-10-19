07b6038688495b47e4ffe59fb9cbd8a3740c2a1d
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-10-19 15:07:13 +0100

    Added counter functionality.

--- ticket-tracker/src/components/card/Card.jsx
+++ ticket-tracker/src/components/card/Card.jsx
@@ -1,18 +1,38 @@
 import "./Card.scss";
-import Button from "../button/Button";  
+import Button from "../button/Button";
+import { useState } from "react";
 
 const Card = (props) => {
 
     const { displayValue, cardName, cardJob } = props;
+    const [ counter, setCounter] = useState(displayValue/1);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+    }
+
+    const handleSub = () => {
+        setCounter(counter - 1);
+    }
 
     return(
         <div className="card">
             <h1 className="card__header">
                 {cardName}({cardJob})
             </h1>
-            <Button buttonText = {"+"} buttonValue="add"/>
-            <p className="card_value">{displayValue}</p>
-            <Button buttonText = {"-"} buttonValue="sub"/>
+            <div
+                className="button-wrapper__add"
+                onClick={handleAdd}
+            >
+                <Button buttonText = {"+"} buttonValue="add"/>
+            </div>
+            <p className="card_value">{counter}</p>
+            <div
+                className="button-wrapper__sub"
+                onClick={handleSub}
+            >
+                <Button buttonText = {"-"} buttonValue="sub"/>
+            </div>
         </div>
     );
 };
